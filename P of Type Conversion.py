b6dfbc1fab19ba1c7438fc40a28099e886a20ac2
LU-House <60131103+LU-House@users.noreply.github.com>
2020-11-01 16:25:14 +0900

    TC stuff

---
+++ P of Type Conversion.py
@@ -0,0 +1,6 @@
+number_1 = input("First: ")
+number_2 = input("Second: ")
+
+print(float(number_1) + int(number_2))
+
+
---
+++ type conversion.py
@@ -0,0 +1,14 @@
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(age)
+
+
+int() #Integer. For example: 10, 1, 2, 3
+float()  #Decimals. For example: 2.4 , 10.1
+bool()
+str()
+
+
+#birth_year = input("Enter your birth year: ")
+#age = 2020 - birth_year
+#print(age)
